3e51efc0efd63d2dadc6f7f90fbeb746f783b266
DIALLO <mohamed-abdoulaye.diallo@capgemini.com>
2023-02-16 12:11:11 +0100

    delete comment

--- exercice/src/test/java/TestJunit.java
+++ exercice/src/test/java/TestJunit.java
@@ -11,21 +11,12 @@ public class TestJunit{
 		Flux<String> source = Flux.just("John", "Monica", "Mark", "Cloe", "Frank", "Casper", "Olivia", "Emily", "Cate")
 				.filter(name -> name.length() == 4).map(String::toUpperCase);
 
-		
 		Flux<String> requetes = Flux.just("close","close");
 		
 		Mono<Boolean> allClose = requetes.all(s-> s.equals("close"));
 		Mono<Boolean> hasElement = requetes.hasElements();
 		
 		Mono<Boolean> zip = Mono.zip(allClose, hasElement).map(tuple-> tuple.getT1() && tuple.getT2());
-		
-//		StepVerifier
-//		  .create(source)
-//		  .expectNext("JOHN")
-//		  .expectNextMatches(name -> name.startsWith("MA"))
-//		  .expectNext("CLOE", "CATE")
-//		  .expectComplete()
-//		  .verify();
 		
 		StepVerifier
 		  .create(zip)
@@ -41,9 +32,6 @@ public class TestJunit{
 		Flux<String> requests = Flux.just("close","open");
 		
 		Mono<Boolean> noRequestOrAllNotClose = requests.all(s-> !s.equals("close"));
-		//Mono<Boolean> hasElement = requests.hasElements();	
-		//Mono<Boolean> zip = Mono.zip(allNotClose, hasElement).map(tuple-> tuple.getT1() && tuple.getT2());
-		
 		Boolean active = true;
 		StepVerifier
 		  .create(noRequestOrAllNotClose)
@@ -59,8 +47,6 @@ public class TestJunit{
 		Flux<String> requests = Flux.just();
 		
 		Mono<Boolean> noRequestOrAllNotClose = requests.all(s-> !s.equals("close"));
-		//Mono<Boolean> hasElement = requests.hasElements();
-		//Mono<Boolean> zip = Mono.zip(allNotClose, hasElement).map(tuple-> tuple.getT1() && tuple.getT2());
 		Boolean active = true;
 		StepVerifier
 		  .create(noRequestOrAllNotClose)
